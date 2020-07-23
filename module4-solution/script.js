543f657ab6366c8ecada8eaec61da6f088068757
mokshit4 <65014835+mokshit4@users.noreply.github.com>
2020-07-23 12:32:08 +0530

    my first javascript project

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -41,7 +41,6 @@ WARNING!!! WARNING!!!
 // 'speak' method.
 // See Lecture 50, part 1
   for (var i=0; i < names.length; i++) {
-
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
